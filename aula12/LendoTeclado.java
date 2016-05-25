56540e40b69fbb31cd1dc40214b6c2c3c79729d2
Akkamai Shami <akkamaishami@gmail.com>
2016-05-25 12:26:27 -0300

    mudanças de rotina

--- aula12/LendoTeclado.java
+++ aula12/LendoTeclado.java
@@ -7,10 +7,9 @@ public class LendoTeclado {
 	private static Scanner scan;
 
 	public static void main(String[] args) {
-		
+
 		scan = new Scanner (System.in);
-					
-		
+
 		System.out.println("Digite os dados como se pede: ");
 		System.out.print("Digite seu nome: ");
 		String nome = scan.next();
@@ -18,13 +17,7 @@ public class LendoTeclado {
 		int idade = scan.nextInt();
 		System.out.print("Digite sua altura: ");
 		float altura = scan.nextFloat();
-		
-		
-		
-		
-		
-		System.out.println("Os dados digitados foram: " +nome+ " " +idade+ " " +altura);
-		
-	}
 
+		System.out.println("Os dados digitados foram: " +nome+ " " +idade+ " " +altura);
+	}
 }
